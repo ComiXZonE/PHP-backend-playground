46e2747b275bfd6cb5ee5b14d187ad307471f582
ComiXZonE <trifundroid94@gmail.com>
2015-12-04 14:45:55 +0100

    write_php 2

--- write.php
+++ write.php
@@ -1,13 +1,21 @@
-<FORM action="http://somesite.com/prog/adduser" method="post">
-    <P>
-    <LABEL for="firstname">First name: </LABEL>
-              <INPUT type="text" id="firstname"><BR>
-    <LABEL for="lastname">Last name: </LABEL>
-              <INPUT type="text" id="lastname"><BR>
-    <LABEL for="email">email: </LABEL>
-              <INPUT type="text" id="email"><BR>
-    <INPUT type="radio" name="sex" value="Male"> Male<BR>
-    <INPUT type="radio" name="sex" value="Female"> Female<BR>
-    <INPUT type="submit" value="Send"> <INPUT type="reset">
-    </P>
- </FORM>
+<!DOCTYPE html>
+<html>
+<body>
+
+date: <input type="text" name="input_date">
+close: <input type="text" name="input_close">
+
+<button onclick="myFunction()">Submit</button>
+
+<p id="demo"></p>
+
+<?php
+function myFunction() {
+    var d = document.getElementById("input_date").value;
+    var c = document.getElementById("input_close").value;
+    document.getElementById("demo").innerHTML = x;
+}
+?>
+
+</body>
+</html>
