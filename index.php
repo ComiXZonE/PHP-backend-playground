a26fdbe510a1c420d9a568d6369c50111004428c
olza <olzagit@gmail.com>
2015-12-04 13:17:49 +0100

    Novi

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+
 <?php 
-for ($x = 0; $x <= 100; $x++) {
-    echo "Olga The number is: $x <br>";
+for ($x = 0; $x <= 10; $x++) {
+     echo "The number is: $x <br>";
 } 
-?>
+?> 
+ 
+ </body>
+</html> 
